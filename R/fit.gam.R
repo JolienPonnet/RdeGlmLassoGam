1c211ab5bd172307da2b25cddd4699b051629cd7
Jolien Ponnet <101267662+JolienPonnet@users.noreply.github.com>
2022-09-02 15:54:31 +0200

    Update fit.gam.R

--- R/fit.gam.R
+++ R/fit.gam.R
@@ -1,3 +1,8 @@
+##' Function used to determine initial GAM estimate
+##' @param Ry the response vector.
+##' @param RB the model matrix.
+##' @param RrS penalty matrix.
+##' @param Rfamily  a character string indicating the family. This can be "poisson" or "binomial".
 fit.gam.sp1 <- function (Ry, RB, RrS, Rfamily)
 .Call("fit_gam_sp_cpp", Ry, RB, RrS, Rfamily, PACKAGE = "RdeGlmLassoGam")
 
